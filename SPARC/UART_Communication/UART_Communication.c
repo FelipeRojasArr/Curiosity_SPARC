7c21dd27a3445cfc069aec60e13d33ffeb5c4038
Felipe Rojas <A01566041@itesm.mx>
2019-11-19 10:10:40 -0700

    UART configuration

--- SPARC/UART_Communication/UART_Communication.c
+++ SPARC/UART_Communication/UART_Communication.c
@@ -17,3 +17,26 @@ void main(void) {
 	Communication_BITsConfiguration();
 	return;
 }
+
+
+void Communication_BITsConfiguration(void){
+	ADCON1 = 0x0F; 						//	COnfigurar los pin a digitales
+    LATB=0; 									//	Los pines b inicializados en 0
+    TRISB=0x00; 							//	Todo el puerto B es salida
+    
+    TRISCbits.RC6 = 0; 					//  Pin RC6 como salida digital para TX. Master/slave Mode Asíncrono
+    TRISCbits.RC7 = 1; 					//  Pin RC7 como entrada digital para RX
+    TXSTAbits.TX9 = 0; 					//  Transmision de 8 bits
+	TXSTAbits.TXEN = 1; 				//  Habilita Transmisión.
+    TXSTAbits.SYNC = 0; 				//  Modo-Asíncrono	Full Duplex
+    TXSTAbits.BRGH = 1; 				//  Modo-Alta Velocidad de Baudios.
+    RCSTAbits.SPEN = 1; 				//  Habilita el Módulo SSP como UART. Serial Port Enable
+	RCSTAbits.RX9 = 0; 					//  Recepcion de 8 bits 
+    
+    BAUDCONbits.BRG16 = 1; 			//  Baudios modo-16bits.
+    
+    RCSTAbits.CREN = 1; 				//	 Habilita recibir
+													//  Escribe el valor necesario para configurar los Baudios a 9600.
+    SPBRGH1:SPBRG1 = 103;
+	return;
+}
