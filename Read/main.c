46e5e2867b313795573b6934b9cbb44545f07ef1
Angie Luna <angieluna.755@gmail.com>
2019-11-21 12:19:58 -0700

    Ahora es un loop

--- Read/main.c
+++ Read/main.c
@@ -1,37 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <stdbool.h>
+
+
 int main()
 {
     uint8_t rv; //receive value
 
     printf("Press I if you want to initiate\n" );
     //aqui iria la lectura serial
+   while(1){
     scanf("%c", &rv );
    // printf("%d", rv);
 
-        if(rv==73 || rv == 105) {
+        if((rv==73 || rv == 105)) {
             coordinates();
+
         }
 
         else{
             printf("\nwaiting...");
         }
-
+   }
 }
 
 coordinates(){
     uint8_t buffer [6];
     uint8_t read;
-    for(int i=0; i<=7; i++){
+    printf("\nCoordenada x:");
+    for(int i=0; i<=8; i++){
         scanf("%c", &read);
         buffer[i]=read-48;
+        if(i==4){
+            printf("\nCoordenada y:");
+        }
     }
-    uint16_t x = 1*buffer[1] + 10*buffer[2] + 100*buffer[3];
-    uint16_t y = 1*buffer[4] + 10*buffer[5] + 100*buffer[6];
+    uint16_t x = 1*buffer[3] + 10*buffer[2] + 100*buffer[1];
+    uint16_t y = 1*buffer[7] + 10*buffer[6] + 100*buffer[5];
 
    printf("\nCoordenada en x= %d", x);
    printf("\nCoordenada en y= %d", y);
 
 }
+
