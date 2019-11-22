c86a3c17767107a265e283fc76f37bc19955729c
Angie Luna <angieluna.755@gmail.com>
2019-11-22 14:45:57 -0700

    saque a x y y de la funcion con apuntadores

--- SPARC/UART_Communication/main.c
+++ SPARC/UART_Communication/main.c
@@ -1,12 +1,40 @@
+// Modified program using pointers
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
 
+// add is the short name for address
+void coord(int* x, int* y)
+{
+	*x=301;
+	*y=301;
 
+	while(*x>=300 || *y>=300){
+	// a is stored in the address pointed
+		// by the pointer variable *add_great
+		uint8_t buffer [8];
+        uint8_t read;
+        printf("\nCoordenada x:");
+        for(int i=0; i<=7; i++){
+            scanf("%c", &read);
+            buffer[i]=read-48;
+            if(i==4){
+                printf("\nCoordenada y:");
+            }
+        }
+		*x = 1*buffer[3] + 10*buffer[2] + 100*buffer[1];
+		*y = 1*buffer[7] + 10*buffer[6] + 100*buffer[5];
+
+		if(*x>300 || *y>300){ printf("\nError. Ingrese de nuevo.\n");}
+	}
+
+}
+
+// Driver code
 int main()
 {
-    uint8_t rv; //receive value
-
+     uint8_t rv; //receive value
+    int cx;
+    int cy;
     printf("Press I if you want to initiate\n" );
     //aqui iria la lectura serial
    while(1){
@@ -14,7 +42,9 @@ int main()
    // printf("%d", rv);
 
         if((rv==73 || rv == 105)) {
-            coordinates();
+        coord(&cx, &cy);
+        printf("\nCoordenada en x= %d", cx);
+        printf("\nCoordenada en y= %d", cy);
 
         }
 
@@ -22,23 +52,6 @@ int main()
             printf("\nwaiting...");
         }
    }
-}
-
-coordinates(){
-    uint8_t buffer [6];
-    uint8_t read;
-    printf("\nCoordenada x:");
-    for(int i=0; i<=8; i++){
-        scanf("%c", &read);
-        buffer[i]=read-48;
-        if(i==4){
-            printf("\nCoordenada y:");
-        }
-    }
-    uint16_t x = 1*buffer[3] + 10*buffer[2] + 100*buffer[1];
-    uint16_t y = 1*buffer[7] + 10*buffer[6] + 100*buffer[5];
-
-   printf("\nCoordenada en x= %d", x);
-   printf("\nCoordenada en y= %d", y);
 
 }
+
