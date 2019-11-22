e9bec879d8ddecaafc0700d4adfa661080fd4bdc
Felipe Rojas <A01566041@itesm.mx>
2019-11-22 11:45:53 -0700

    EEPROM Read and Write, Bit configuration

--- SPARC/EEPROM/EEPROM.c
+++ SPARC/EEPROM/EEPROM.c
@@ -7,30 +7,41 @@
 
 
 #include <xc.h>
+#include "EEPROM.h"
 
 void main(void) {
-    return;
-}
-
-int Read_EEPROM(int AddressToRead){
-    EEADR = AddressToRead;               //read from what address
-    EECON1bits.EEPGD = 0;               //Determines if the access will be to program or data memory
-    EECON1bits.RD = 1;     
-    EECON1bits.CFGS = 0;
     
     return;
 }
 
+unsigned char Read_EEPROM(int AddressToRead){
+    EEADR = AddressToRead;                  //Read from what address
+    EECON1bits.EEPGD = 0;                   //Determines if the access will be to program or data memory
+    EECON1bits.CFGS = 0;                    //Access EEPROM
+    EECON1bits.RD = 1;                      //EEPROM Read
+    EEDATA = contentOfAddress;              //Copies content
+    
+    return contentOfAddress;                
+}
 
-int Write_EEPROM(char ContentToWrite, int AddressToWrite){
-    EEADR = AddressToWrite;                 //
-    EEDATA = ContentToWrite;
-    EECON1bits.EEPGD = 0;                   // EEPGD = 0, access data EEPROM memory
-    EECON1bits.CFGS = 0;                    //Access Flash program or data memory EEPROM
+
+void Write_EEPROM(char ContentToWrite, int AddressToWrite){
+    EEADR = AddressToWrite;                 //Assign the address
+    EEDATA = ContentToWrite;                //Writes the desire content 
+    EECON1bits.EEPGD = 0;                   //EEPGD = 0, Access data EEPROM memory
+    EECON1bits.CFGS = 0;                    //CFGS = 0, Access Flash program or data memory EEPROM
     EECON1bits.WREN = 1;                    //WREN = 1, Allows write cycles to data EEPROM
-    EECON2 = 0x55;
+    INTCONbits.GIE = 0;                     //Disables interruptions
+    EECON2 = 0x55;                          //Values needed defined by datasheet
     NOP();
-    EECON2 = 0xAA;
-    EECON1bits.WR = 1;
-    
+    EECON2 = 0x0AA;                         //Values needed defined by datasheet
+    EECON1bits.WR = 1;                      //Write control bit
+    INTCONbits.GIE = 1;                     //Enables interruptions
+    while(PIR2bits.EEIF == 0)               //Interruption Flag for EEPROM. If equal 0, write hasn't been completed
+    {
+        ;;
+    }
+    EECON1bits.WREN = 0;                    //End of Write
+    return;
 }  
+
