fdb73a4218b4e9b7dff1f381256b6949886408e7
Angie Luna <angieluna.755@gmail.com>
2019-11-25 10:48:25 -0700

    ya no hay errores

--- SPARC/UART_Communication/main.c
+++ SPARC/UART_Communication/main.c
@@ -1,44 +1,83 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <xc.h>
 #include <stdint.h>
 
+#define  BAUD 9600
+#define _XTAL_FREQ  4000000 //Frecuencia de 4MHz  
 
-int main()
-{
+void UARTconfi (void); 
+uint8_t UARTRead(void);
+void UARTWrite(uint8_t txData);
+void BITSCONFI(uint8_t X);
+
+unsigned char init=0;//caracter de inicializacion
+unsigned char Enviar;
+
+void main(void) {
+    OSCCON = 0x60; //Reloj interno a 4MHz
+    UARTconfi (); //9600 BAUD, Tranmitir y recibir
     uint8_t rv; //receive value
-
-    printf("Press I if you want to initiate\n" );
-    //aqui iria la lectura serial
-   while(1){
-    scanf("%c", &rv );
-   // printf("%d", rv);
+    int cx;
+    int cy;
+    while(1){
+        rv= UARTRead();//scanf("%c", &rv );
 
         if((rv==73 || rv == 105)) {
-            coordinates();
+
+            coord(&cx, &cy);
+        //printf("\nCoordenada en x= %d", cx);
+        //printf("\nCoordenada en y= %d", cy);
 
         }
-
-        else{
+       /* else{
             printf("\nwaiting...");
-        }
+        }*/
    }
 }
 
-coordinates(){
-    uint8_t buffer [6];
-    uint8_t read;
-    printf("\nCoordenada x:");
-    for(int i=0; i<=8; i++){
-        scanf("%c", &read);
-        buffer[i]=read-48;
-        if(i==4){
-            printf("\nCoordenada y:");
-        }
+void UARTconfi (void) { 
+    ADCON1 = 0x0F; //COnfigurar los pin a digitales
+    LATB=0; //Los pines b inicializados en 0
+    TRISB=0x00; //Todo el puerto B es salida
+    
+    TRISCbits.RC6 = 0; //  Pin RC6 como salida digital para TX.
+    TRISCbits.RC7 = 1; //  Pin RC7 como entrada digital para RX.
+    
+    TXSTAbits.TX9 = 0; // Transmision de 8 bits
+    RCSTAbits.RX9 = 0; //Recepcion a 8 bits 
+    TXSTAbits.TXEN = 1; //  Habilita Transmisi?n.
+    TXSTAbits.SYNC = 0; //  Modo-As?ncrono	Full Duplex
+    TXSTAbits.BRGH = 1; //  Modo-Baja Velocidad de Baudios.
+    BAUDCONbits.BRG16 = 1; //  Baudios modo-16bits.
+    RCSTAbits.SPEN = 1; //  Hbilita el M?dulo SSP como UART.
+    RCSTAbits.CREN = 1; //Habilita recibir
+    //  Escribe el valor necesario para configurar los Baudios a 9600.
+    SPBRGH1:SPBRG1 = 103;
+}
+
+uint8_t UARTRead(void) {
+    uint8_t X;
+    if (PIR1bits.RCIF == 1){ // Si RCREG esta lleno (llego informacion)
+        X = RCREG1; // se guarda lo que llego de RCREG1 en la variable de inicio
+        RCREG1 = 0; //Se resetea el registro recibidor
     }
-    uint16_t x = 1*buffer[3] + 10*buffer[2] + 100*buffer[1];
-    uint16_t y = 1*buffer[7] + 10*buffer[6] + 100*buffer[5];
+    return X;
+}
 
-   printf("\nCoordenada en x= %d", x);
-   printf("\nCoordenada en y= %d", y);
+coord(int* x, int* y){
+	*x=301;
+	*y=301;
 
+	while(*x>300 || *y>300){ //para que se repita si x o y son mayores a 300
+		uint8_t buffer [8];
+        uint8_t read;
+        for(int i=0; i<=7; i++){
+            read=UARTRead();//scanf("%c", &read);
+            buffer[i]=read-48;
+        }
+        
+		*x = 1*buffer[3] + 10*buffer[2] + 100*buffer[1];
+		*y = 1*buffer[7] + 10*buffer[6] + 100*buffer[5];
+
+		//if(*x>300 || *y>300){ printf("\nError. Ingrese de nuevo.\n");}
+        }
 }
